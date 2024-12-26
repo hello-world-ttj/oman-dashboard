3d9eb072e82a8fd309686eb60144c7dd13f02a97
hello-world-ttj <tijotjoseph003@gmail.com>
2024-12-26 17:21:29 +0530

    #image

--- Oman-frontend/src/ui/StyledCropImage.jsx
+++ Oman-frontend/src/ui/StyledCropImage.jsx
@@ -171,7 +171,7 @@ export const StyledCropImage = ({ label, value, onChange, ratio }) => {
             image={imageForCrop}
             crop={crop}
             zoom={zoom}
-            aspect={2500 / 2800}
+            aspect={2500 / 3000}
             onCropChange={setCrop}
             onZoomChange={setZoom}
             onCropComplete={onCropComplete}
